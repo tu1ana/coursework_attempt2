db900e1eeaa1f77b3bbb5ca4ba3bdb0772ddacb8
Tuyana Chirninova <hikkonki@gmail.com>
2023-06-20 01:50:19 +0300

    add get_exec_transaction func

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,8 @@ def load_operations():
     with open('operations.json', encoding='utf-8') as file:
         operations = json.load(file)
         return operations
+
+
+def get_exec_transaction(list_):
+    sorted_list = [x for x in list_ if 'state' in x and x['state'] == 'EXECUTED']
+    return sorted_list
