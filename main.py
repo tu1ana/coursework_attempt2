00b2fa514a60f2a33456ce75a4f308a23a7c2ec3
Tuyana Chirninova <hikkonki@gmail.com>
2023-06-24 16:22:07 +0300

    changes to main

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ last_transactions = 5
 def main():
     init_list = load_operations()
     sorted_list = get_exec_transaction(init_list)
-    # print(len(init_list), len(sorted_list))
-    # print(get_last_transactions(sorted_list, last_transactions))
     data = get_last_transactions(sorted_list, last_transactions)
-    # print(display_data(data))
     data = display_data(data)
+
     for line in data:
         print(line, end='\n\n')
 
